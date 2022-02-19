f5ea576feff69aae134479990ea40e536909fe0d
Tidbits JS <tidbitsjs@gmail.com>
2022-02-19 22:52:26 +0530

    moving ball using reanimated

---
+++ src/HeartOfTheMatter/Reanimated.tsx
@@ -0,0 +1,56 @@
+import { StyleSheet, View } from "react-native";
+import type { PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
+
+import { StyleGuide } from "../components";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  ball: {
+    backgroundColor: StyleGuide.palette.primary,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+  },
+});
+
+export const Reanimated = () => {
+  const x = useSharedValue(0);
+  const y = useSharedValue(0);
+  
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    { x: number; y: number }
+  >({
+    onStart: (_, ctx) => {
+      ctx.x = x.value;
+      ctx.y = y.value;
+    },
+    onActive: ({ translationX, translationY }, ctx) => {
+      x.value = ctx.x + translationX;
+      y.value = ctx.y + translationY;
+    },
+  });
+  
+  const style = useAnimatedStyle(() => ({
+    transform: [{ translateX: x.value }, { translateY: y.value }],
+  }));
+  //useMakeJsThreadBusy();
+
+  return (
+    <View style={styles.container}>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View style={[styles.ball, style]} />
+      </PanGestureHandler>
+    </View>
+  );
+};
