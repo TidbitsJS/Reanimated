53a6ef1afd0d08c1dd246c914044561a6b680606
Tidbits JS <tidbitsjs@gmail.com>
2022-02-28 23:23:01 +0530

    bouncing card

--- src/PanGesture/PanGesture.tsx
+++ src/PanGesture/PanGesture.tsx
@@ -1,6 +1,15 @@
 import { View, StyleSheet } from "react-native";
+import Animated, {
+  useAnimatedGestureHandler,
+  useSharedValue,
+  useAnimatedStyle,
+  withDecay,
+} from "react-native-reanimated";
+import type { PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import { clamp, withBouncing } from "react-native-redash";
 
-import { Card, Cards } from "../components";
+import { Card, Cards, CARD_WIDTH, CARD_HEIGHT } from "../components";
 
 const styles = StyleSheet.create({
   container: {
@@ -14,10 +23,59 @@ interface GestureProps {
 }
 
 export const PanGesture = ({ width, height }: GestureProps) => {
-  console.log({ width, height });
+  const boundX = width - CARD_WIDTH;
+  const boundY = height - CARD_HEIGHT;
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    {
+      offsetX: number;
+      offsetY: number;
+    }
+  >({
+    onStart: (_, ctx) => {
+      ctx.offsetX = translateX.value;
+      ctx.offsetY = translateY.value;
+    },
+    onActive: (event, ctx) => {
+      translateX.value = clamp(ctx.offsetX + event.translationX, 0, boundX);
+      translateY.value = clamp(ctx.offsetY + event.translationY, 0, boundY);
+    },
+    onEnd: ({ velocityX, velocityY }) => {
+      translateX.value = withBouncing(
+        withDecay({
+          velocity: velocityX,
+        }),
+        0,
+        boundX
+      );
+      translateY.value = withBouncing(
+        withDecay({
+          velocity: velocityY,
+        }),
+        0,
+        boundY
+      );
+    },
+  });
+
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <Card card={Cards.Card1} />
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View {...{ style }}>
+          <Card card={Cards.Card1} />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
